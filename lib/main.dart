fabffb1af02000c04db73d892dc51936af71d4a0
Icyy <adityaauty9@gmail.com>
2021-12-10 13:25:05 +0530

    developed more on the app added custom widget, statefulWidget and styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,48 @@
+import 'package:first_app/question.dart';
 import "package:flutter/material.dart";
 
-
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  void onPressedA(){
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  var questions = [
+    "What is your favorite color",
+    "what is your favorite animal",
+    "what is your favorite cartoon",
+    "what is your favorite video game",
+  ];
+
+  void onPressedA() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
     print("Button was pressed!");
   }
+
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(home : Scaffold(
-      appBar: AppBar(
-        title: Text("First App"),
-      ),
-      body: Column(children: [
-        Text("What is your quesiton"),
-        RaisedButton(child:Text("option 1"),onPressed: onPressedA),
-        RaisedButton(child:Text("option 2"),onPressed: onPressedA),
-        RaisedButton(child:Text("option 3"),onPressed: onPressedA)
-      ],)
-    ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("First App"),
+          ),
+          body: Column(
+            children: [
+              Question(questions[questionIndex]),
+              RaisedButton(child: Text("option 1"), onPressed: onPressedA),
+              RaisedButton(child: Text("option 2"), onPressed: onPressedA),
+              RaisedButton(child: Text("option 3"), onPressed: onPressedA)
+            ],
+          )),
     );
   }
 }
---
+++ lib/question.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import "package:flutter/material.dart";
+
+class Question extends StatelessWidget {
+  String questionText;
+  Question(this.questionText, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
